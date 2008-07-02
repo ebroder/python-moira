e6fe336920eabbed0c00f88730b861cfe5344169
Evan Broder <broder@mit.edu>
2008-07-02 02:34:59 -0400

    I think this is worthy of a version number now

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Pyrex.Distutils import build_ext
 
 setup(
     name="PyMoira",
-    version="",
+    version="1.0.0",
     description="PyMoira - Python bindings for the Athena Moira library",
     author="Evan Broder",
     author_email="broder@mit.edu",
