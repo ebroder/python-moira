649ada5d51163ff3efbebed385a3c221c6a8e083
Evan Broder <broder@mit.edu>
2008-07-02 02:31:18 -0400

    Queries work!

--- moira.pyx
+++ moira.pyx
@@ -7,16 +7,17 @@ cdef extern from "moira/moira.h":
     int mr_motd(char ** motd)
     int mr_noop()
     int mr_query(char * handle, int argc, char ** argv,
-                 int (*callback)(int, char **, void *), void * callarg)
-    int mr_version(int version)
+                 int (*callback)(int, char **, object), object callarg)
 
-class ConnectionException(Exception):
+cdef extern from "stdlib.h":
+    ctypedef unsigned long size_t
+    void * malloc(size_t size)
+    void free(void * ptr)
+
+class MoiraException(Exception):
     pass
 
-class AuthenticationException(Exception):
-    pass
-
-connected = False
+__connected = False
 
 def connect(server=''):
     """
@@ -30,28 +31,28 @@ def connect(server=''):
     MOIRASERVER environment variable, Hesiod, or a compiled in default
     (in that order).
     
-    This function raises a ConnectionException if the connection is
+    This function raises a MoiraException if the connection is
     not successful.
     """
-    global connected
+    global __connected
     
-    if connected:
+    if __connected:
         disconnect()
     
     status = mr_connect(server)
     if status != 0:
-        raise ConnectionException, status
+        raise MoiraException, status
     else:
-        connected = True
+        __connected = True
 
 def disconnect():
     """
     Disconnect from the active Moira server
     """
-    global connected
-    if connected:
+    global __connected
+    if __connected:
         mr_disconnect()
-    connected = False
+    __connected = False
 
 def auth(program, krb4=False):
     """
@@ -71,4 +72,68 @@ def auth(program, krb4=False):
     else:
         status = mr_krb5_auth(program)
     if status != 0:
-        raise AuthenticationException, status
+        raise MoiraException, status
+
+def host():
+    """
+    Return the name of the host the client is connected to.
+    """
+    cdef char buffer[512]
+    status = mr_host(buffer, 512)
+    if status != 0:
+        raise MoiraException, status
+    return buffer
+
+def motd():
+    """
+    Retrieve the current message of the day from the server.
+    """
+    cdef char * motd
+    status = mr_motd(&motd)
+    if status != 0:
+        raise MoiraException, status
+    if motd != NULL:
+        return motd
+
+def noop():
+    """
+    Does "no operation" to the server, just making sure it's still
+    there
+    """
+    status = mr_noop()
+    if status:
+        raise MoiraException, status
+
+cdef int _query_callback(int argc, char ** argv, object results):
+    result = []
+    cdef int i
+    for i in xrange(argc):
+        result.append(argv[i])
+    results.append(tuple(result))
+    return 0
+
+def query(handle, *args):
+    """
+    Execute a Moira query and return the result as a list of tuples.
+    
+    All of the real work of Moira is done in queries. There are over
+    100 queries, each of which requires different arguments. The
+    arguments to the queries should be passed as separate arguments to
+    the function.
+    """
+    cdef int argc, i
+    argc = len(args)
+    cdef char ** argv
+    argv = <char **>malloc(argc * sizeof(char *))
+    
+    if argv != NULL:
+        for i in xrange(argc):
+            argv[i] = args[i]
+        
+        result = []
+        status = mr_query(handle, argc, argv, _query_callback, result)
+        free(argv)
+        
+        if status:
+            raise MoiraException, status
+        return result
