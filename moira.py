e05fd1a4147fdd00163e4a5e2c93db2a865227a6
Evan Broder <broder@mit.edu>
2009-11-13 17:14:22 -0500

    Replace the crappy object-threading-and-queue-based Python query handlers with much simpler, unthreaded functions.
    Moira seems to mostly just batch up all of its return values at the
    end, which means there's no performance benefit from trying to
    dispatch return values as quickly as possible.
    
    And the UX of having to re-cast the return value from moira.query to a
    list really sucked.

--- moira.py
+++ moira.py
@@ -6,100 +6,54 @@ central repository for information about users, groups hosts, print
 queues, and several other aspects of the Athena environment.
 """
 
+import re
+
 import _moira
 from _moira import connect, disconnect, auth, host, motd, noop
 
-import re
-import threading
-from Queue import Queue
 
 help_re = re.compile('([a-z0-9_, ]*) \(([a-z0-9_, ]*)\)(?: => ([a-z0-9_, ]*))?',
                      re.I)
 
-class __Handler(object):
-    def __init__(self, handle, *args, **kwargs):
-        self.handle = handle
-        self.args = args
-        self.kwargs = kwargs
-        self.results = []
-        
-        self.q = Queue()
-        self.worker = threading.Thread(target=self.do_query)
-        
-        self.setup()
-        self.worker.start()
-    
-    def do_query(self):
-        try:
-            _moira._query(self.handle, self.callback, *self.args)
-        except Exception, e:
-            self.q.put(e)
-        self.q.put(None)
-        self.cleanup()
-    
-    def callback(self, result):
-        self.q.put(result)
-        self.results.append(result)
-    
-    def __iter__(self):
-        i = 0
-        while True:
-            item = self.q.get()
-            if item is None:
-                break
-            elif isinstance(item, Exception):
-                raise item
-            else:
-                yield item
-    
-    def cleanup(self):
-        pass
-    
-    def setup(self):
-        pass
 
-class __SmartHandler(__Handler):
-    # Because these variables aren't redefined by __init__, they are
-    # shared by all instances of __SmartHandler
-    arg_cache = dict()
-    return_cache = dict()
-    
-    def setup(self):
-        if self.handle not in self.return_cache or \
-                self.return_cache[self.handle] is None:
-            self.__load_help()
-        if self.kwargs != dict():
-            self.args = tuple(self.kwargs.get(i, '*') \
-                                  for i in self.arg_cache[self.handle])
-    
-    def __load_help(self):
-        help_string = ', '.join(list(_list_query('_help', self.handle))[0]).strip()
-        
-        handle_str, arg_str, return_str = help_re.match(help_string).groups('')
-        
-        handles = handle_str.split(', ')
-        args = arg_str.split(', ')
-        returns = return_str.split(', ')
-        
-        for h in handles:
-            self.arg_cache[h] = args
-            self.return_cache[h] = returns
-    
-    def callback(self, result):
-        dict_result = dict()
-        map(dict_result.__setitem__, self.return_cache[self.handle], result)
-        self.q.put(dict_result)
-        self.results.append(dict_result)
+_arg_cache = {}
+_return_cache = {}
 
-def _list_query(*args, **kwargs):
+
+def _load_help(handle):
+    """Fetch info about the arguments and return values for a query.
+
+    This uses the "_help" Moira query to retrieve names for the
+    arguments and return values to and from a particular Moira
+    query. These values are cached and used for translating arguments
+    and return values into and out of dictionaries and into and out of
+    tuples.
+    """
+    help_string = ', '.join(query('_help', handle)[0]).strip()
+
+    handle_str, arg_str, return_str = help_re.match(help_string).groups('')
+
+    handles = handle_str.split(', ')
+    args = arg_str.split(', ')
+    returns = return_str.split(', ')
+
+    for h in handles:
+        _arg_cache[h] = args
+        _return_cache[h] = returns
+
+
+def _list_query(handle, *args):
     """
     Execute a Moira query and return the result as a list of tuples.
     
     This bypasses the tuple -> dict conversion done in moira.query()
     """
-    return __Handler(*args, **kwargs)
+    results = []
+    _moira._query(handle, results.append, *args)
+    return results
 
-def query(*args, **kwargs):
+
+def query(handle, *args, **kwargs):
     """
     Execute a Moira query and return the result as a list of dicts.
     
@@ -112,10 +66,26 @@ def query(*args, **kwargs):
     arguments to the queries should be passed as separate arguments to
     the function.
     """
-    if args[0].startswith('_'):
-        return _list_query(*args, **kwargs)
+    if handle.startswith('_'):
+        return _list_query(handle, *args, **kwargs)
     else:
-        return __SmartHandler(*args, **kwargs)
+
+        if handle not in _return_cache or \
+                not _return_cache[handle]:
+            _load_help(handle)
+
+        if kwargs:
+            args = tuple(kwargs.get(i, '*') \
+                             for i in _arg_cache[handle])
+
+        plain_results = _list_query(handle, *args, **kwargs)
+        results = []
+
+        for r in plain_results:
+            results.append(dict(zip(_return_cache[handle], r)))
+
+        return results
+
 
 __all__ = ['connect', 'disconnect', 'auth', 'host', 'motd', 'noop', 'query',
            '_list_query']
