66cf90e5696e3f50c860dd8b76261e17bf7d9212
Evan Broder <broder@mit.edu>
2009-11-19 11:09:05 -0500

    Release version 4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from Pyrex.Distutils import build_ext
 
 setup(
     name="PyMoira",
-    version="4.0",
+    version="4.1",
     description="PyMoira - Python bindings for the Athena Moira library",
     author="Evan Broder",
     author_email="broder@mit.edu",
